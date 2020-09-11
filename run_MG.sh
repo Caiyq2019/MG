86ad8caee95d4d515428c41b6877afbc90bf3584
Caiyq <47945531+Caiyq2019@users.noreply.github.com>
2020-09-11 19:49:05 +0800

    Update run_MG.sh

--- run_MG.sh
+++ run_MG.sh
@@ -1,1 +1,1 @@
-python train.py --epochs 200 --num-blocks 10  --batch-size 100  --lr 0.0005  --logpz 0.0 --logdet 1.0 --L2_SW 10 --cos_SW 10 --L2_SB 10  --cos_SB 500  --gpu 0
+python train.py --epochs 200 --num-blocks 10  --batch-size 20  --lr 0.0005  --logpz 0.0 --logdet 1.0 --L2_SW 10 --cos_SW 10 --L2_SB 10  --cos_SB 500  --gpu 0
